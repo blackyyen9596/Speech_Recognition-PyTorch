e8cf2e1a763e784d84c03c15d775b6382d9ad9ff
Chen-Yen Chang <66304059+BlackyYen@users.noreply.github.com>
2021-01-10 23:28:19 +0800

    Create remove_maxlength_audio.py

---
+++ remove_maxlength_audio.py
@@ -0,0 +1,17 @@
+import librosa
+import os
+from tqdm import tqdm
+
+path = r'D:\dataset\ntut-ml-2020-spring-taiwanese-e2e\train_org'
+filelist = os.listdir(path)
+maxduration = 10
+for filename in (filelist):
+    file_path = os.path.join(path, filename)
+    y, sr = librosa.load(file_path)
+    duration = librosa.get_duration(y, sr)
+    if duration > 20:
+        maxduration = duration
+        filepath = os.path.join(path, filename)
+        os.remove(filepath)
+        # print(maxduration)
+        print(filename)
